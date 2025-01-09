afdaae7780af1c3beeca4dfbaf8529b58228fc30
urielbang <uri32655@gmail.com>
2025-01-09 22:43:34 +0200

    add client

---
+++ client/src/components/PostCreate.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function PostCreate() {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+    console.log(res.data);
+    setTitle("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="">Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
