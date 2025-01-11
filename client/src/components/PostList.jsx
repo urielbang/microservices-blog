8b9a15e7d686df4f0e626cd1cd3fd5cc6aad4eea
urielbang <uri32655@gmail.com>
2025-01-11 22:53:30 +0200

    bus event

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -15,26 +15,24 @@ export default function PostList() {
     fetchPosts();
   }, []);
 
-  const renderedPosts = Object.values(posts).map((post) => {
-    return (
-      <div
-        className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
-        key={post.id}
-      >
-        <div className="card-body">
-          <h3>{post.title}</h3>
-        </div>
-        <CommentList postId={post.id} />
-
-        <CommentCreate postId={post.id} />
-      </div>
-    );
-  });
-
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">
-      {renderedPosts}
+      {Object.values(posts).map((post) => {
+        return (
+          <div
+            className="card"
+            style={{ width: "30%", marginBottom: "20px" }}
+            key={post.id}
+          >
+            <div className="card-body">
+              <h3>{post.title}</h3>
+            </div>
+            <CommentList postId={post.id} />
+
+            <CommentCreate postId={post.id} />
+          </div>
+        );
+      })}
     </div>
   );
 }
