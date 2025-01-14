d286604214560017212ebbb83d8a75d9a1a81e03
urielbang <uri32655@gmail.com>
2025-01-14 18:38:08 +0200

    add query

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,6 +11,7 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4000/events", event); // posts sever
   axios.post("http://localhost:4001/events", event); // comments server
   axios.post("http://localhost:4002/events", event); // query server
+  axios.post("http://localhost:4003/events", event); // moderation server
 
   res.send({ status: "ok" });
 });
--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,25 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.include("orange") ? "rejected" : "approved";
+
+    // to bus
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId.app,
+        status,
+        content: data.content,
+      },
+    });
+  }
+  res.send({});
+});
 
 app.listen(4003, () => {
   console.log("listening on 4003");
